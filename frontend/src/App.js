50435ccc8ca680750e8e7002b7a60cdf51fc7322
JanneKH <janne.hauglid@gmail.com>
2021-10-26 10:16:00 +0200

    added service for polls and votes, and pollcompoment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PollComponent from "./component/PollComponent";
 import UserComponent from "./component/UserComponent";
 
 export default function App() {
@@ -14,12 +15,18 @@ export default function App() {
                         <li>
                             <Link to="/users">Users</Link>
                         </li>
+                        <li>
+                            <Link to="/polls">Polls</Link>
+                        </li>
                     </ul>
                 </nav>
 
                 {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
                 <Switch>
+                    <Route path="/polls">
+                        <PollComponent />
+                    </Route>
                     <Route path="/users">
                         <UserComponent />
                     </Route>
---
+++ frontend/src/component/PollComponent.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import PollService from '../services/PollService';
+
+function PollComponent() {
+
+    const [polls, setPolls] = useState([])
+
+    useEffect(() => {
+        getPolls()
+    }, [])
+
+    const getPolls = () => {
+
+        PollService.getPolls().then((response) => {
+            setPolls(response.data)
+            console.log(response.data);
+        });
+    };
+
+    return (
+        <div className = "container">
+            <h1 className = "text-center"> Poll List</h1>
+            <table className = "table table-striped">
+                <thead>
+                    <tr>
+                        <th> Poll Id</th>
+                        <th> User User ID</th>
+                        <th> Poll Question</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        polls.map(
+                                poll =>
+                                <tr key = {poll.id}>
+                                    <td> {poll.id }</td>
+                                    <td> {poll.user_id.id }</td>
+                                    <td> {poll.question }</td>    
+                                </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PollComponent
