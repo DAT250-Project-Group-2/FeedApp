4c7434072372fcb740dc8af1b6b49c7e67e6a1c5
JanneKH <janne.hauglid@gmail.com>
2021-11-16 21:12:07 +0100

    updated navbar

--- frontend/src/component/AppNavbar.js
+++ frontend/src/component/AppNavbar.js
@@ -8,15 +8,15 @@ function AppNavbar() {
         <Navbar.Brand href="/">FeedApp</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          {/* <Nav.Link href="/users">Users</Nav.Link>
-          <Nav.Link href="/polls">Polls</Nav.Link> */}
+          <Nav.Link href={"/profile/" + localStorage.getItem("userID")}>Profile</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="/register">Register</Nav.Link>
           {(localStorage.getItem("userID") === null) ?
+          <Nav>
+          <Nav.Link href="/register">Register</Nav.Link>
            <Nav.Link href="/login">Login</Nav.Link> 
-           : <Nav> <Nav.Link onClick={() => localStorage.clear()} href="/">Logout</Nav.Link>
-             <Nav.Link href={"/profile/" + localStorage.getItem("userID")}>Profile</Nav.Link> </Nav>  }
+          </Nav> : <Nav> <Nav.Link onClick={() => localStorage.clear()} href="/">Logout</Nav.Link>
+          </Nav>  }
         </Nav>
       </Container>
     </Navbar>
